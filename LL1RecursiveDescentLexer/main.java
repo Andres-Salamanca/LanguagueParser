20811638f96b7c898e3cbd329de9f8f5a68443ff
Andres-Salamanca <andrealebarbaritos@gmail.com>
2023-06-28 23:41:32 -0500

    nested list

--- LL1RecursiveDescentLexer/main.java
+++ LL1RecursiveDescentLexer/main.java
@@ -3,7 +3,7 @@ package LL1RecursiveDescentLexer;
 public class main {
     public static void main(String[] args) {
 
-        ListLexer lexer = new ListLexer("[a, b ,1,2,3,4]");
+        ListLexer lexer = new ListLexer("[a, b ,1,2,3,4,[2,3]]");
         Token t = lexer.nextToken();
         while ( t.type != Lexer.EOF_TYPE ) {
             System.out.println(t);
